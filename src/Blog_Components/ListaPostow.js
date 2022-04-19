29430a5d7245effb96875b3a2ef0d514d8c92b44
RamzesX <norbert.marchewka44@gmail.com>
2022-04-19 23:26:25 +0200

    Update ListaPostow.js

--- src/Blog_Components/ListaPostow.js
+++ src/Blog_Components/ListaPostow.js
@@ -62,7 +62,7 @@ class ListaPostow extends React.Component {
   }
 
   changeContent(pageNumber) {
-    fetch("http://51.68.142.75:2369/ghost/api/v3/content/posts?key=f854db3727ebf596c066859b88&limit=4&filter=tag:" + this.getTag(this.state.url) +"&page=" + (pageNumber + 1).toString())
+    fetch("https://ghost.techautority.tech/ghost/api/v3/content/posts?key=f854db3727ebf596c066859b88&limit=4&filter=tag:" + this.getTag(this.state.url) +"&page=" + (pageNumber + 1).toString())
     .then((res) => res.json())
     .then((json) => {
       var posty2 = this.transformPost(json.posts);
